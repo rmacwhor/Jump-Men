2b20f81be827e2707919bc67892283bde02edf2f
Brian Khuu <bhkhuu@uci.edu>
2021-02-03 19:25:39 -0800

    Made it so camera teleports along with player to spawn

--- Assets/Scripts/destroy.cs
+++ Assets/Scripts/destroy.cs
@@ -9,7 +9,8 @@ public class destroy : MonoBehaviour
     public GameObject Player2;
     public Transform Spawn1;
     public Transform Spawn2;
-
+    public GameObject Camera1;
+    public GameObject Camera2;
 
 
 
@@ -26,9 +27,16 @@ public class destroy : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.name == "Player")
+        {
             Player1.transform.position = Spawn1.transform.position;
+            Camera1.transform.position = new Vector3(Spawn1.position.x, Spawn1.position.y, Camera1.transform.position.z);
+            
+        }
         else if (other.name == "Player2")
+        {   
             Player2.transform.position = Spawn2.transform.position;
+            Camera2.transform.position = new Vector3(Spawn2.position.x, Spawn2.position.y, Camera2.transform.position.z);
+        }
 
         Debug.Log("KillZone collided with " + other.name);
     }
