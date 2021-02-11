16ebbc42bd4caad23c3c3ab3f9a2f6bfd81c6b6b
jerccolfer <50119363+jerccolfer@users.noreply.github.com>
2021-02-10 19:56:44 -0800

    Moving platforms added
    The moving platform script takes in a distance to move, a speed, and a bool determining vertical or horizontal movement

---
+++ Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    public float distance = 10f;
+    public float speed = 2f;
+    public bool vertical = false;
+    private Vector3 startPos;
+    private Vector3 nextPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        if (vertical)
+        {
+            nextPos = startPos;
+            nextPos.y += distance;
+        }
+        else
+        {
+            nextPos = startPos;
+            nextPos.x += distance;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position == nextPos)
+        {
+            Vector3 temp = startPos;
+            startPos = nextPos;
+            nextPos = temp;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+}
