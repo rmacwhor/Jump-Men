f7ee391113771b78d714fe82c0d4442c4e537e87
Brian Khuu <bhkhuu@uci.edu>
2021-02-22 12:59:54 -0800

    Added finish line that disables both players

---
+++ Assets/Scripts/GoalBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalBehavior : MonoBehaviour
+{
+
+    public GameObject player1;
+    public GameObject player2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Game Ended");
+        player1.GetComponent<PrototypeHeroDemo>().isDisabled = true;
+        player2.GetComponent<PrototypeHeroDemo>().isDisabled = true;
+    }
+}
