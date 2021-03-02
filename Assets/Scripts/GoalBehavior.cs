0df1c277d937a4b861b1dcc295eb887bce9f2a84
Ryan M <rmacwhor@uci.edu>
2021-03-01 16:12:45 -0800

    Players will now return to main menu after goal is reached

--- Assets/Scripts/GoalBehavior.cs
+++ Assets/Scripts/GoalBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoalBehavior : MonoBehaviour
 {
@@ -25,5 +26,14 @@ public class GoalBehavior : MonoBehaviour
         Debug.Log("Game Ended");
         player1.GetComponent<PrototypeHeroDemo>().isDisabled = true;
         player2.GetComponent<PrototypeHeroDemo>().isDisabled = true;
+
+        // Wait before loading back to menu.
+        StartCoroutine(OnGoal());
+    }
+
+    private IEnumerator OnGoal()
+    {
+        yield return new WaitForSecondsRealtime(3.0f);
+        SceneManager.LoadScene("Menu");
     }
 }
