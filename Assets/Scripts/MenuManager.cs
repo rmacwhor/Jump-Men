a9447e8a847fe95510494a337747c2ad03012e0c
Ryan M <rmacwhor@uci.edu>
2021-03-15 18:16:29 -0700

    Added level selection/back button to menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,8 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    private GameObject playMenu;
+    private GameObject levelMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playMenu = GameObject.Find("Play Screen");
+        levelMenu = GameObject.Find("Level Selection Screen");
+        levelMenu.SetActive(false);
+    }
+
     public void Play()
     {
-        SceneManager.LoadScene(1);
+        playMenu.SetActive(false);
+        levelMenu.SetActive(true);
     }
+
+    public void GoBack()
+    {
+        levelMenu.SetActive(false);
+        playMenu.SetActive(true);
+    }
+
+    public void LoadLevel(int levelNum)
+    {
+            SceneManager.LoadScene(levelNum);
+    }
+
 }
